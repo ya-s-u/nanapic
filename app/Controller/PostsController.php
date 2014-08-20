d39c1f1244db916f739ab58820b96e1dcec4fee0
Goto Yasuaki <yasu1003@gmail.com>
2014-08-20 18:33:20 +0900

    記事投稿サーバー側設計

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -5,6 +5,7 @@ class PostsController extends AppController {
 
     public function beforeFilter() {
         parent::beforeFilter();
+        $this->Auth->allow(array('create'));
     }
 
     /**
@@ -21,17 +22,45 @@ class PostsController extends AppController {
     public function create() {
         $this->set('title_for_layout', '新規投稿');
         
-        //ファイルから取得
-		$array = file_get_contents('http://api.nanapi.jp/v1/recipeLookupDetails/?recipe_id=118233&key=4cb94f0895324&format=json');
-			$array = json_decode($array);
-			print_r($array->responce);
+        if ($this->request->isPost()) {
         
-        //記事詳細を取得
+        	//サムネイルURLを取るためnanapiAPIにアクセス
+        	$recipe_id = $this->request->data['thumb'];
+	        $data = file_get_contents('http://api.nanapi.jp/v1/recipeLookupDetails/?recipe_id='.$recipe_id.'&key=4cb94f0895324&format=json');
+			$data = json_decode($data, true);
         
-        //dbに保存
-		//$this->Post->createPost();
-		
-		//リダイレクト
+        	//記事情報をdbに保存
+        	$post = array(
+        		'user_id' => $this->Auth->user('id'),
+        		'title' => $this->request->data['title'],
+        		'comment' => $this->request->data['comment'],
+        		'thumb_img_url' => $data['response']['recipe']['image'],
+        	);
+        	$this->Post->cretaePost();
+
+	        //レシピデータを取得しdbに保存
+	        for ($i=0; $i<10; $i++) {
+	        	$recipe_id = $this->request->data['recipe'.$i];
+	        
+	        	//nanapiAPIにアクセス
+	        	$data = file_get_contents('http://api.nanapi.jp/v1/recipeLookupDetails/?recipe_id='.$recipe_id.'&key=4cb94f0895324&format=json');
+				$data = json_decode($data, true);
+				
+				//post_linkに保存
+				$recipe = array(
+					'post_id' => $post_id,
+					'order' => $i,
+					'title' => $data['response']['recipe']['title'],
+					'description' => $data['response']['recipe']['description'],
+					'thumb_img_url' => $data['response']['recipe']['image'],
+					'nanapi_article_id' => $recipe_id,
+				);
+				$this->Post->createPost($recipe);
+	        }
+			
+			//リダイレクト
+			$this->redirect(array('controller'=>'users','action'=>'index'));
+		}
     }
 
 }
